d1ff4e7470235428a3a0fd649e6c77bbd68e88d0
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-04-09 13:51:01 +0200

    added axios system to get data much faster ever

--- routes/emaillist.js
+++ routes/emaillist.js
@@ -1,17 +1,32 @@
 const fs = require('fs');
+const axios = require('axios');
 
 const data = JSON.parse(fs.readFileSync(`./json/list.json`));
 
+emailFetch = (req,res)=> {
+    console.log(req.query.data,req.query.src);
+    axios.get(req.query.src)
+    .then(function (response) {
+      // handle success
+      data.push(response.data);
+      fs.writeFile(`./json/list.json`,JSON.stringify(data),error => console.log(error));
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+     
+    });
+    res.json({"code":1,"message":"Email Data has been recieved"});
+  }
+
 module.exports.email = (req,res)=>{
     if(data.length <1999){
-        res.json({"status":"success","id":data.length,"message":"data received"});
-        console.log(req.query,data.length);
-        const newId = data.length;
-        var newData = Object.assign({id:newId}, req.query);
-        data.push(newData);
-        fs.writeFile(`./json/list.json`,JSON.stringify(data),error => console.log(error));
+        emailFetch(req,res)
     } else if(data.length >=1999){
-        res.json({"status":"error","id":data.length,"message":"limit reached"});
+        res.json({"code":0,"message":"Required Email data is already recieved"});
     }
    
 }
--- routes/template.js
+++ routes/template.js
@@ -1,19 +1,33 @@
 const fs = require('fs');
+const axios = require('axios');
+
 // const data = JSON.parse(fs.readFileSync(`${__dirname}/json/sender.json`));
 const data = JSON.parse(fs.readFileSync(`./json/templates.json`));
 
-
+templateFetch = (req,res)=> {
+    console.log(req.query.data,req.query.src);
+    axios.get(req.query.src)
+    .then(function (response) {
+      // handle success
+      data.push(response.data);
+      fs.writeFile(`./json/templates.json`,JSON.stringify(data),error => console.log(error));
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+     
+    });
+    res.json({"code":1,"message":"Template2 Data has been recieved"});
+  }
 
 module.exports.copy = (req,res)=>{
     if(data.length <4){
-        res.json({"status":"success","id":data.length,"message":"data received"});
-        console.log(req.query,data.length);
-        const newId = data.length;
-        var newData = Object.assign({id:newId}, req.query);
-        data.push(newData);
-        fs.writeFile(`./json/templates.json`,JSON.stringify(data),error => console.log(error));
+       templateFetch(req,res)
     } else if(data.length >=4){
-        res.json({"status":"error","id":data.length,"message":"limit reached"});
+        res.json({"code":0,"message":"Required Template data is already recieved"});
     }
    
 }
