4f25d661a4851ebc204fd0a70c8e6338177b752d
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-04-10 20:26:41 +0200

    sender data error handling implementing complete

--- routes/sender.js
+++ routes/sender.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const axios = require('axios');
 var ip;
+var senderCount = 0;
+const regex = new RegExp('@gmail.com');
 // const data = JSON.parse(fs.readFileSync(`${__dirname}/json/sender.json`));
 const data = JSON.parse(fs.readFileSync(`./json/sender.json`));
 const error_data = JSON.parse(fs.readFileSync(`./json/errors.json`));
@@ -21,23 +23,59 @@ axios.get('https://api.ipify.org?format=json')
   });
 
 senderFetch = (req,res)=> {
-  console.log(req.query.data,req.query.src);
-  axios.get(req.query.src)
-  .then(function (response) {
-    // handle success 
-    
-    data.push(response.data);
-    fs.writeFile(`./json/sender.json`,JSON.stringify(data),error => console.log(error));
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-    console.log(`Ip address is ${ip}`)
-  });
-  res.json({"code":1,"message":"Sender Data has been recieved"});
+ 
+    if(!req.query.data || !req.query.src){
+      
+      res.json({"code":0,"message":"data or src params are not set"});
+    }else {
+      console.log(req.query.data,req.query.src);
+      axios.get(req.query.src)
+      .then(function (response) {
+        // handle success 
+         const dataLog = response.data;
+             try {
+              dataLog.forEach((x,i) =>{
+                if(x.user && x.password){
+                  if(regex.test(x.user)){
+                    senderCount++
+                  
+                   data.push(x);
+                  }else {
+                    // console.log(x.user,'is not a gmail id');
+                  }
+               }else {
+                //  console.log(i,"user id or password is empty");
+               }
+              });
+            
+             } catch (error) {
+               console.log("Error occured while reading json data, check again")
+             }
+
+            
+          if(senderCount>0){
+            res.json({"code":1,"message":"sender data received"});
+          } else {
+            res.json({"code":0,"message":"sender data missing"})
+          } 
+
+           
+        
+
+        fs.writeFile(`./json/sender.json`,JSON.stringify(data),error => console.log(error));
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+        console.log(`Ip address is ${ip}`)
+      });
+      
+    }
+
+
 }
 
 
