d2b5251b41878b7e6f1dc45ab47ef6b10db8419c
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-04-28 19:27:45 +0200

    updated random template module

--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -38,10 +38,10 @@ var temp;
       
         console.log("Message sent: %s", info.messageId);
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        status.writeStatus(`Message sent: %s : ${info.messageId},Preview URL: %s : ${nodemailer.getTestMessageUrl(info)}`);
+        status.writeStatus(`Message sent: %s : ${info.messageId},Preview URL: %s : ${nodemailer.getTestMessageUrl(info)},user: ${user},email: ${email},template:${template.subject}`);
     
         // Preview only available when sending through an Ethereal account
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info),"user:",user,"email:",email,"template:",template.subject);
     
         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...  
   } catch (error) {
